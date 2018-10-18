df660c496fdfbd0dbd7192143c4e13f5f32612e4
GERMAIN ARTURO DURAN TORRES <arturodurantorres@gmail.com>
2018-10-18 15:57:37 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,19 @@
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  languages.each do |style, language|
+    language.each do |language_name, attribute|
+      if new_hash[language_name] == nil
+          new_hash[language_name] = attribute
+        end
+      new_hash[language_name][:style] = []
+      new_hash[language_name][:style] << style
+    end
+  end
+  new_hash[:javascript][:style] << :oo
+  new_hash
 end
+
+
+
+
+ 
