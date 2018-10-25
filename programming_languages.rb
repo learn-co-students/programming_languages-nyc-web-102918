eb4c72f8ae8b54362ce35f146fe704e55efd4ebd
lysistrata1989 <ehamiltonhudson@gmail.com>
2018-10-25 12:37:00 -0400

    Done

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,39 +1,7 @@
 require 'pry'
-languages_by_style = {
-  :oo => {
-    :ruby => {
-      :type => "interpreted"
-    },
-    :javascript => {
-      :type => "interpreted"
-    },
-    :python => {
-      :type => "interpreted"
-    },
-    :java => {
-      :type => "compiled"
-    }
-  },
-  :functional => {
-    :clojure => {
-      :type => "compiled"
-    },
-    :erlang => {
-      :type => "compiled"
-    },
-    :scala => {
-      :type => "compiled"
-    },
-    :javascript => {
-      :type => "interpreted"
-    }
-
-  }
-}
 
 def reformat_languages(languages)
   new_hash = {}
-
   languages.each do |style, language|
       language.each do |name, data|
         data.each do |type, value|
